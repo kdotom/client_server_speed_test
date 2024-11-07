198f9e4c2515a2c1533935c247e7a99c9f369742
kdotom <kopongmensah@gmail.com>
2024-11-07 10:42:58 -0500

    Update shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -1,1 +1,4 @@
+# Simple shell script which copies a file to an IP + specific directory over a network connection.
+# Note: ssh should be enabled for the target machine.
+
 rsync --progress -h filename user@address:/path/for/file
